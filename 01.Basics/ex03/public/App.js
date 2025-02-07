39cadfdbce955036062e279607d32a4d9c5a902e
BIT <>
2025-02-07 17:08:59 +0900

    react #2 02/07

---
+++ 01.Basics/ex03/public/App.js
@@ -0,0 +1,8 @@
+function App(){
+    const img = document.createElement('div');
+    App.textContent = "hi";
+
+    return App;
+}
+
+export {App};
---
+++ 01.Basics/ex05/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(){
+//    const img = document.createElement('div');
+//    App.textContent = "hi";
+
+    const App = React.createElement('div',null,'Hello World');
+    
+    return App;
+
+
+}
+
+export {App};
