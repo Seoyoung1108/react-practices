ee5a8ef9884ef1789655603d1dd9021ebaa101b0
BIT <>
2025-02-10 13:12:29 +0900

    react #3 02/10

--- 01.Basics/ex07/src/index.js
+++ 01.Basics/ex07/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
+console.log(progress.env.NODE_ENV);
+if(progress.env.NODE_ENV==="production"){
+  console.log = () => {};
+  console.info = () => {};
+  console.error = () => {};
+  console.warn = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
