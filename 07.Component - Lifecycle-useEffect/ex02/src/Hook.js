bd52058cb9e1156ac83975aebce70eb82718ffa5
BIT <>
2025-02-12 16:54:01 +0900

    react #10 02/12

--- 07.Component - Lifecycle-useEffect/ex02/src/Hook.js
+++ 07.Component - Lifecycle-useEffect/ex02/src/Hook.js
@@ -7,23 +7,38 @@ export default function Hook({ color }) {
     /**
      *   [1] Alternative 01: getDerivedStateFromProps
      */
-
+    if(boxColor!==color){
+        setBoxColor(color);
+    }
 
     /**
      *   [2-1] After Rendering 함수 - Alternative 02: componentDidUpdate
-     */
+     
+    useEffect(()=>{
+        console.log('After Rendering');
+    })
+    */
+
     /**
      *  [2-2] After Rendering 함수 - 어떤 특정 상태(boxColor)의 변화에 반응(관심 분리)
-     */
-    
-    
+     
+    useEffect(()=>{
+        console.log('After Rendering');
+    },[boxColor, title])
+    */
 
     /**
      *  [3] Alternative 03: componentDidMount & componentWillUnmount
      */
+    useEffect(()=>{
+        // mount 후 실행
+        console.log('After Mount(componentDidMount)');
 
-
-
+        // unmount 전 실행
+        return ()=>{
+            console.log('Before Unmount(componentWillUnmount)');
+        }
+    },[])
 
 
     // render
--- 07.Component - Lifecycle-useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle-useEffect/ex04/src/App.js
@@ -1,9 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    function getCurrentTime(){
+        const now = new Date();
+    
+        return{
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+        }
+    }
+
+    const [currentTime, setCurrentTime] = useState(getCurrentTime);
+    const [ticks, setTicks] = useState(0);
+
+    const useInterval = (callback, delay) => {
+        const savedCallback = useRef(null);
+    
+        useEffect(() => {
+          savedCallback.current = callback;
+        }, [callback]);
+    
+        useEffect(() => {
+          const executeCallback = () => {
+            savedCallback.current();
+          };
+    
+          const timerId = setInterval(executeCallback, delay);
+    
+          return () => clearInterval(timerId);
+        }, []);
+      };
+    
+      useInterval(() => {
+        setCurrentTime(getCurrentTime);
+        setTicks(ticks+1);
+      }, 1000);
+
     return (
+        ticks % 5 === 0? null:
         <Clock
             title={`ex04: Clock Component II: ${ticks}`}
             hours={currentTime.hours}
