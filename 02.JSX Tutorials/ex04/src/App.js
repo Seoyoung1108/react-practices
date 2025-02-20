63fc92548683830fa026dca37b7670278036112b
BIT <>
2025-02-20 09:19:32 +0900

    react #7 02/11

--- 02.JSX Tutorials/ex04/src/App.js
+++ 02.JSX Tutorials/ex04/src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import Header from './Header';
 import Contents from './Contents';
 
 function App() {
-    /*
+    const refDiv = useRef(null); //current:null
+    const [test, setTest] = useState('hello');
+
+    
     return (
-        <div>
+        /*<div>
             <Header />
             <Contents title="함수 컴포넌트" />
+        </div>*/
+        <div id="App" ref={refDiv}>
+            {'Test01'}
         </div>
     );
-    */
+
+    /*
    return React.createElement('div',{
     id:'App'
-   }, React.createElement(Header));
+   }, React.createElement(Header));*/
 }
 
 export {App};
